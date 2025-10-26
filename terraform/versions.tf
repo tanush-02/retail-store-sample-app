ad23b5d81d971745bea71e1b6ab11f7c84881b76
Tanush <tanushr20@gmail.com>
2025-10-26 19:57:35 +0530

    Add GitHub Actions workflows and IAM configuration

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -38,7 +38,7 @@ provider "aws" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes{
     host                   = module.retail_app_eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
     exec {
